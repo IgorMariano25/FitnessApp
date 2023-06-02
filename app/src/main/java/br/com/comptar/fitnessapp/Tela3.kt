98db088c18e56b11d869af9b442972a216cfa51b
Igor Mariano <igor-mariano@outlook.com>
2023-06-02 18:58:15 -0300

    Adicionando estrutura básica para outras telas
    Tela2, Tela3, Tela4 e Tela5

---
+++ app/src/main/java/br/com/comptar/fitnessapp/Tela3.kt
@@ -0,0 +1,13 @@
+package br.com.comptar.fitnessapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Tela3 : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
+    }
+}
