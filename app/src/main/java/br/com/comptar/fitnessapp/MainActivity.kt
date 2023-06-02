fa8eeb2b30f90560e7eab1ec0537e81ab328260a
Igor Mariano <igor-mariano@outlook.com>
2023-06-02 18:08:52 -0300

    Layout visual primeira tela finalizado !

--- app/src/main/java/br/com/comptar/fitnessapp/MainActivity.kt
+++ app/src/main/java/br/com/comptar/fitnessapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package br.com.comptar.fitnessapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var exerciseCount = 0
+    private lateinit var exerciseCountTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        exerciseCountTextView = findViewById(R.id.exerciseCountTextView)
+        val incrementButton: Button = findViewById(R.id.incrementButton)
+        val resetButton: Button = findViewById(R.id.resetButton)
+
+        incrementButton.setOnClickListener {
+            exerciseCount++
+            updateExerciseCount()
+        }
+
+        resetButton.setOnClickListener {
+            exerciseCount = 0
+            updateExerciseCount()
+        }
+    }
+
+    private fun updateExerciseCount() {
+        exerciseCountTextView.text = exerciseCount.toString()
     }
 }
