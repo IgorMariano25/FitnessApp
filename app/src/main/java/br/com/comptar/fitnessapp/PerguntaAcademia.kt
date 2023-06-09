6e823ff56d05cabc54aec36e46d6e710ff8ac536
Igor Mariano <igor-mariano@outlook.com>
2023-06-09 20:09:56 -0300

    Alterando cor do botão quando o usuário clicar nele

--- app/src/main/java/br/com/comptar/fitnessapp/PerguntaAcademia.kt
+++ app/src/main/java/br/com/comptar/fitnessapp/PerguntaAcademia.kt
@@ -1,13 +1,69 @@
 package br.com.comptar.fitnessapp
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class PerguntaAcademia : AppCompatActivity() {
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_pergunta_academia)
+
+    private lateinit var btnRespostaNenhumaAcademia: Button
+    private lateinit var btnRespostaEntre_1_vez_e_2_vezes_Academia: Button
+    private lateinit var btnRespostaEntre_2_vezes_e_3_vezes_Academia: Button
+    private lateinit var btnRespostaEntre_3_vezes_e_4_vezes_Academia: Button
+    private lateinit var btnRespostaMaisDe4Vezes_Academia: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pergunta_academia)
+
+        // Criando uma nova cor para adicionar ao botão
+        val cor_cinza_btn_clicado = Color.rgb(97, 97, 97)
+
+        // Alterando entre as telas ao clicar no botão de confirmar resposta
+        val btnConfirmaRespostaActivityPergunta1 = findViewById<Button>(R.id.btnFragmentoConfResp)
+        btnConfirmaRespostaActivityPergunta1.setOnClickListener {
+            val intent = Intent(this@PerguntaAcademia, PerguntaAgua::class.java)
+            startActivity(intent)
+        }
+
+        // Altera a cor do botão com opção NENHUMA, caso seja a resposta selecionada
+        val btnRespostaNenhumaAcademia = findViewById<Button>(R.id.btnRespostaNenhumaAcademia)
+        btnRespostaNenhumaAcademia.setOnClickListener {
+            btnRespostaNenhumaAcademia.setBackgroundColor(cor_cinza_btn_clicado)
+            btnRespostaNenhumaAcademia.setTextColor(Color.WHITE)
+        }
+
+        // Altera a cor do botão com opção 1 VEZ - 2 VEZES, caso seja a resposta selecionada
+        val btnRespostaEntre_1_vez_e_2_vezes_Academia =
+            findViewById<Button>(R.id.btnRespostaEntre_1_vez_e_2_vezes_Academia)
+        btnRespostaEntre_1_vez_e_2_vezes_Academia.setOnClickListener {
+            btnRespostaEntre_1_vez_e_2_vezes_Academia.setBackgroundColor(cor_cinza_btn_clicado)
+            btnRespostaEntre_1_vez_e_2_vezes_Academia.setTextColor(Color.WHITE)
+        }
+
+        // Altera a cor do botão com opção 2 VEZES - 3 VEZES, caso seja a resposta selecionada
+        val btnRespostaEntre_2_vezes_e_3_vezes_Academia =
+            findViewById<Button>(R.id.btnRespostaEntre_2_vezes_e_3_vezes_Academia)
+        btnRespostaEntre_2_vezes_e_3_vezes_Academia.setOnClickListener {
+            btnRespostaEntre_2_vezes_e_3_vezes_Academia.setBackgroundColor(cor_cinza_btn_clicado)
+            btnRespostaEntre_2_vezes_e_3_vezes_Academia.setTextColor(Color.WHITE)
+        }
+
+        // Altera a cor do botão com opção 3 VEZES - 4 VEZES, caso seja a resposta selecionada
+        val btnRespostaEntre_3_vezes_e_4_vezes_Academia =
+            findViewById<Button>(R.id.btnRespostaEntre_3_vezes_e_4_vezes_Academia)
+        btnRespostaEntre_3_vezes_e_4_vezes_Academia.setOnClickListener {
+            btnRespostaEntre_3_vezes_e_4_vezes_Academia.setBackgroundColor(cor_cinza_btn_clicado)
+            btnRespostaEntre_3_vezes_e_4_vezes_Academia.setTextColor(Color.WHITE)
+        }
+
+        // Altera a cor do botão com opção MAIS DE 4 VEZES, caso seja a resposta selecionada
+        val btnRespostaMaisDe4Vezes_Academia =
+            findViewById<Button>(R.id.btnRespostaMaisDe4Vezes_Academia)
+        btnRespostaMaisDe4Vezes_Academia.setOnClickListener {
+            btnRespostaMaisDe4Vezes_Academia.setBackgroundColor(cor_cinza_btn_clicado)
+            btnRespostaMaisDe4Vezes_Academia.setTextColor(Color.WHITE)
         }
     }
 }
