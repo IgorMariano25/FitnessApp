9f305642c844653ad2e3bf92efaba9712ba21fae
Igor Mariano <igor-mariano@outlook.com>
2023-06-10 11:22:38 -0300

    Alterando poucas coias na MainActivity

--- app/src/main/java/br/com/comptar/fitnessapp/MainActivity.kt
+++ app/src/main/java/br/com/comptar/fitnessapp/MainActivity.kt
@@ -8,18 +8,18 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
-class PerguntaNome : AppCompatActivity(){
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pergunta_nome)
+        setContentView(R.layout.activity_main)
 
     val btnContinuar: Button = findViewById(R.id.btnContinuar)
     btnContinuar.setOnClickListener {
         val editTextNomeInformado: EditText = findViewById(R.id.editTextNomeInformado)
         val nomeInformado = editTextNomeInformado.text.toString().trim()
         if (nomeInformado.isNotEmpty() && nomeInformado != "Por favor, insira um nome !" ){
-            val intent = Intent(this@PerguntaNome, MainActivity::class.java)
+            val intent = Intent(this@MainActivity, BoasVindas::class.java)
             intent.putExtra("name", nomeInformado)
             startActivity(intent)
         } else {
@@ -31,7 +31,6 @@ class PerguntaNome : AppCompatActivity(){
             editTextNomeInformado.setOnClickListener {
                 editTextNomeInformado.setText("")
                 editTextNomeInformado.setTextColor(Color.BLACK)
-                editTextNomeInformado.setTypeface(null, Typeface.BOLD_ITALIC)
                 editTextNomeInformado.setTextSize(24f)
                 }
             }
