3a37ed1c11f7556ce745459959834f6579c9caf4
Igor Mariano <igor-mariano@outlook.com>
2023-06-13 22:03:30 -0300

    Criando classe FragmentoBotao.kt

---
+++ app/src/main/java/br/com/comptar/fitnessapp/FragmentoBotao.kt
@@ -0,0 +1,11 @@
+package br.com.comptar.fitnessapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FragmentoBotao : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragmento_botao)
+    }
+}
